17eeaf0af470c336705742f148045b781f889da3
Nick Plekhanov <nickatgit@yandex.com>
2017-04-12 16:35:21 +0300

    Initial setup and create User model

---
+++ app.js
@@ -0,0 +1,109 @@
+const fs = require('fs'),
+const http = require('http'),
+const path = require('path'),
+const methods = require('methods'),
+const express = require('express'),
+const bodyParser = require('body-parser'),
+const morgan = require('morgan');
+const session = require('express-session'),
+const cors = require('cors'),
+const passport = require('passport'),
+const errorhandler = require('errorhandler'),
+const mongoose = require('mongoose');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+// Models
+require('./models/User');
+
+// Create global app object
+const app = express();
+const db = mongoose.connection;
+
+// App configuration
+
+// Set static files location
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Use body parser, so we can grab information from POST requests
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Configure our app to handle CORS requests
+app.use(cors());
+
+// Log all requests to the console
+app.use(morgan('dev'));
+
+app.use(require('method-override')());
+
+app.use(session({
+  secret: 'medium-lite',
+  cookie: { maxAge: 60000 },
+  resave: false,
+  saveUninitialized: false
+}));
+
+if (!isProduction) {
+  app.use(errorhandler());
+}
+
+// DB connection handlers
+db.on('error', (error) => {
+  console.error('mongoose connection: ERROR');
+  throw new Error(error);
+});
+
+db.on('open', () => {
+  console.error('mongoose connection: OPEN');
+});
+
+// Connect to our database
+if (isProduction) {
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  mongoose.connect('mongodb://localhost/ledium');
+  mongoose.set('debug', true);
+}
+
+// App routes
+app.use(require('./routes'));
+
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Error handlers
+
+// Development error handler: will print stacktrace
+if (!isProduction) {
+  app.use((err, req, res, next) => {
+    console.log(err.stack);
+
+    res.status(err.status || 500);
+
+    res.json({
+      'errors': {
+        message: err.message,
+        error: err
+      }
+    });
+  });
+}
+
+// Production error handler: no stacktraces leaked to user
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    'errors': {
+      message: err.message,
+      error: {}
+    }
+  });
+});
+
+// Start the server
+const server = app.listen(process.env.PORT || 2017, () => console.log(`Server running at http://localhost:${server.address().port}/`));
---
+++ models/User.js
@@ -0,0 +1,77 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+
+const appSecret = require('../config').secret;
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    lowercase: true,
+    unique: true,
+    required: [true, 'can\'t be blank'],
+    match: [/^[a-zA-Z0-9]+$/, 'is invalid'],
+    index: true
+  },
+  email: {
+    type: String,
+    lowercase: true,
+    unique: true,
+    required: [true, 'can\'t be blank'],
+    match: [/\S+@\S+\.\S+/, 'is invalid'],
+    index: true
+  },
+  bio: String,
+  image: String,
+  hash: String,
+  salt: String
+}, { timestamps: true });
+
+UserSchema.plugin(uniqueValidator, { message: 'is already taken.' });
+
+/**
+ * Set user password.
+ */
+UserSchema.methods.setPassword = function (password) {
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 100000, 512, 'sha512').toString('hex');
+};
+
+/**
+ * Validate user password.
+ */
+UserSchema.methods.isValidPassword = function (password) {
+  const hash = crypto.pbkdf2Sync(password, this.salt, 100000, 512, 'sha512').toString('hex');
+  return hash === this.hash;
+};
+
+/**
+ * Generate a JWT (JSON Web Token).
+ */
+UserSchema.methods.generateJwt = function () {
+  const today = new Date();
+  const expiresAt = new Date(today);
+
+  // Expires in 60 days in the future
+  expiresAt.setDate(today.getDate() + 60);
+
+  return jwt.sign({
+    id: this._id,
+    username: this.username,
+    expiresAt: parseInt(expiresAt.getTime() / 1000)
+  }, secret);
+};
+
+/**
+ * Get the JSON representation of a user for authentication.
+ */
+UserSchema.methods.toAuthJSON = function () {
+  return {
+    username: this.username,
+    email: this.email,
+    token: this.generateJwt()
+  };
+};
+
+mongoose.model('User', UserSchema);
